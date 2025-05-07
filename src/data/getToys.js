51d1a06eaf6cb6c91de83d0b1ebaccd2644e4e14
Sara Serti <saaris1985@gmail.com>
2025-05-07 14:18:24 +0200

    change price to number in firestore

--- src/data/getToys.js
+++ src/data/getToys.js
@@ -6,7 +6,6 @@ async function getToys(setToys) {
 	const toysCollection = collection(db, 'toys');
 	const toysSnapshot = await getDocs(toysCollection);
 	const toyList = toysSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data(),
-		price: parseFloat(doc.data().price),
 	 }));
 	setToys(toyList);
 	console.log('getToys', toyList)
